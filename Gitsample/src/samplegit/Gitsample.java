20244118723cf99fc2b73cada517994f90e51399
Admin <Admin@Kowsy>
2023-02-28 08:25:30 +0530

    Commit

---
+++ Gitsample/src/samplegit/Gitsample.java
@@ -0,0 +1,12 @@
+package samplegit;
+
+public class Gitsample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=5,b=10,c=a+b;
+System.out.print(c);
+
+	}
+
+}
